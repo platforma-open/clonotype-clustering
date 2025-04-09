484ccea9d83c32f7f25f9277d3465b8488c38ad2
aiturbidemil <aiturbide@milaboratories.com>
2025-04-09 12:38:27 +0200

    wf working not umap

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,11 +1,12 @@
 import { model } from '@platforma-open/milaboratories.clonotype-clustering.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
 import MainPage from './pages/MainPage.vue';
-
+import UMAPPage from './pages/UMAP.vue';
 export const sdkPlugin = defineApp(model, () => {
   return {
     routes: {
       '/': () => MainPage,
+      '/umap': () => UMAPPage,
     },
   };
 });
