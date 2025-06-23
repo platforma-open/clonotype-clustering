2e59ca4d3ec4fbd3c0c8b69aaac94d67956fc72c
lorcai <ilorcaalonso@gmail.com>
2025-06-23 15:49:26 +0200

    change to raw distance

--- software/src/process_results.py
+++ software/src/process_results.py
@@ -306,9 +306,9 @@ else:
     print(f"WARNING: clonotypeKey values in the written {output_distance_tsv} are still not unique. This is unexpected after dropping duplicates.")
 
 # --- Generate cluster-radius.tsv ---
-# Calculate max distance per cluster
-cluster_radius_df = distance_df_to_write.group_by("clusterId").agg(
-    pl.max("distanceToCentroid").alias("clusterRadius")
+# Calculate max raw distance per cluster
+cluster_radius_df = distance_df.group_by("clusterId").agg(
+    pl.max("total_raw_distance").alias("clusterRadius")
 )
 
 # Write to TSV
